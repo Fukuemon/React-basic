c434c3fb57f96eef1abf8501a88e75d00a245ea0
Fukuemon <Fukuemony029@gmail.com>
2023-05-23 14:39:26 +0900

    ⚡️ Basic1のコンポーネントにイベントハンドラーの実装 #1 イベントハンドラ：ベントハンドラはユーザーのアクションに応じて動作する関数 機能：ボタンのクリックに反応する

--- src/components/Basic1.js
+++ src/components/Basic1.js
@@ -1,22 +1,24 @@
-// Reactライブラリをインポート
 import React from 'react'
 
-// Basic1という関数コンポーネントを定義
-// このコンポーネントはpropsというパラメータを受けとる
-// propsは親コンポーネントから子コンポーネントへのデータフローを制御するためのオブジェクト
+
+// Basic1というReactの関数コンポーネント
+// propsは親コンポーネントから子コンポーネントへデータを渡すためのもの
 const Basic1 = (props) => {
-  // JSXを返します。このJSXが実際にブラウザ上でレンダリングされる。
+  // clickHandlerという名前の関数を定義
+  const clickHandler = () => {
+    console.log('clicked')
+  }
+
+  // 表示内容をreturn
+  // 表示する内容には、<button>要素と2つの<h1>要素が含まれる
   return (
-    // フラグメントを使用して複数の要素を一つにまとめまる。
     <>
-      {/* props.nameを使用して名前を表示。 */}
+      <button onClick={clickHandler}>click</button>
       <h1>Hello { props.name }</h1>
-      {/* 単純に文字列"Hello React"を表示 */}
       <h1>Hello React</h1> 
     </>
   )
 }
 
-// このコンポーネントをエクスポート。これにより他のファイルからこのコンポーネントをimportして利用することができる
 export default Basic1
 
